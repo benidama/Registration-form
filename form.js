d6e63f17700ac9486126e6100e06f64288160c5e
benidama <benidama02@gmail.comls>
2024-09-29 21:40:19 +0000

    contact form

--- form.js
+++ form.js
@@ -1,49 +1,50 @@
-const form = document.getElementById("form");
+//Variables for inputs
 const firstName = document.getElementById("firstName");
-const secondName = document.getElementById("secondName");
-const email = document.getElementById("email");
-const genQuery = document.getElementById("gen-query");
-const suppoRequ = document.getElementById("Support-reque");
+const lastName = document.getElementById("lastName");
+const emailAdress = document.getElementById("emailAdress");
+const generalEnquiry = document.getElementById("generalEnquiry");
+const supportRequest = document.getElementById("supportRequest");
 const message = document.getElementById("message");
-const consent = document.getElementById("checkbox");
+const consent = document.getElementById("consentCheckbox");
+const form = document.getElementById("form");
 
-//Errors
-const firstNameError = document.getElementById("firstNameError");
-const secondNameError = document.getElementById("secondNameError");
-const emailError = document.getElementById("emailError");
-const enquiryError = document.getElementById("enquiryError");
-const messageError = document.getElementById("messageError");
-const consentError = document.getElementById("consentError");
+//Variables for errors
+const firstName_error = document.getElementById("firstName_error");
+const lastName_error = document.getElementById("lastName_error");
+const emailAdress_error = document.getElementById("emailAdress_error");
+const enquiry_error = document.getElementById("enquiry_error");
+const message_error = document.getElementById("message_error");
+const consent_error = document.getElementById("consent_error");
 
 //Reactions
 form.addEventListener("submit", (e) => {
   if (firstName.value === "" || firstName.value == null) {
     e.preventDefault();
-    console.log("Please enter your first name");
+    firstName_error.innerHTML = "Please enter your first name";
   }
 
   if (lastName.value === "" || lastName.value == null) {
     e.preventDefault();
-    secondNameError.innerHTML = "Please enter your second name";
+    lastName_error.innerHTML = "Please enter your last name";
   }
 
-  if (email.value === "" || email.value == null) {
+  if (emailAdress.value === "" || emailAdress.value == null) {
     e.preventDefault();
-    emailError.innerHTML = "Please enter valid email address";
+    emailAdress_error.innerHTML = "Please enter your email address";
   }
 
-  if (genQuery.checked === false && suppoRequ.checked === false) {
+  if (generalEnquiry.checked === false && supportRequest.checked === false) {
     e.preventDefault();
-    enquiryError.innerHTML = "Please specify your enquiry";
+    enquiry_error.innerHTML = "Please specify your enquiry";
   }
 
   if (message.value === "" || message.value == null) {
     e.preventDefault();
-    messageError.innerHTML = "Please, leave your comment";
+    message_error.innerHTML = "Please describe your question";
   }
 
   if (consent.checked === false) {
     e.preventDefault();
-    consentError.innerHTML = "please, consent in order to submit this form";
+    consent_error.innerHTML = "Please accept the privacy policy";
   }
 });
